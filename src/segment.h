885edd9dbe809dd217696475cf33b88de3c6db56
gdymind <gdymind@gmail.com>
2023-02-27 16:16:38 -0600

    Use template for keys

--- src/segment.h
+++ src/segment.h
@@ -8,7 +8,7 @@
 
 #include "bucket.h"
 
-
+template<class T>
 class Model {
 public:
     double a = 0.0;
@@ -23,45 +23,45 @@ public:
         b *= expansion_factor;
     }
     
-    inline int predict(key_type key) const {
+    inline int predict(T key) const {
         return static_cast<int>(a * static_cast<double>(key) + b);
     }
 
-    inline double predict_double(key_type key) const {
+    inline double predict_double(T key) const {
         return a * static_cast<double>(key) + b;
     }
 };
 
 
-template<size_t SBUCKET_SIZE>
+template<class T, size_t SBUCKET_SIZE, T MAX_KEY>
 class Segment {
 public:
-    bool is_leaf; // true -> segment; false -> segment group
-    size_t num_bucket; // total num of buckets
+    bool is_leaf_; // true -> segment; false -> segment group
+    size_t num_bucket_; // total num of buckets
 
-    // Segment* parent = nullptr; // the parent Segment node, which enables bottom-up tranversal
-    key_type pivot; // smallest element
-    // key_type base; // key compression
+    Segment* parent_ = nullptr; // the parent Segment node, which enables bottom-up tranversal
+    T pivot_; // smallest element
+    // T base; // key compression
 
-    Bucket<SBUCKET_SIZE>* sbucket_list; // a list of S-Buckets
+    Bucket<T, SBUCKET_SIZE, MAX_KEY>* sbucket_list_; // a list of S-Buckets
 
 
-    uint64_t* lookup(key_type key); //return the child pointer
-    bool insert(KVPTR kvptr); // insert an entry to the target S-Bucket; If the target S-Bucket is full, reblance the bucket with its right neighbor; If bucket_rebalance does not work, insert() return false
+    uint64_t* lookup(T key); //return the child pointer
+    bool insert(KVPTR<T> kvptr); // insert an entry to the target S-Bucket; If the target S-Bucket is full, reblance the bucket with its right neighbor; If bucket_rebalance does not work, insert() return false
 
     
 private:
-    Model model_;
+    Model<T> model_;
 
     void train_model();
 
-    inline unsigned int predict_buck(key_type key) { // get the predicted S-Bucket ID based on the model computing
+    inline unsigned int predict_buck(T key) { // get the predicted S-Bucket ID based on the model computing
         unsigned int buckID = (unsigned int)(model_.predict(key) / SBUCKET_SIZE + 0.5); //TODO: if SBUCKET_SIZE is the power of 2, change division to right shift
-        buckID = std::min(buckID, (unsigned int)(num_bucket-1));
+        buckID = std::min(buckID, (unsigned int)(num_bucket_-1));
         return buckID;
     }
 
-    inline unsigned int locate_buck(key_type key) { // precition may be incorrect, this function is to find the exact bucket whose range covers the key based on prediction
+    inline unsigned int locate_buck(T key) { // precition may be incorrect, this function is to find the exact bucket whose range covers the key based on prediction
         // Step1: call predict_buck to get an intial position
         // Step2: search neighbors to find the exact match
     }
@@ -71,21 +71,21 @@ private:
 };
 
 
-template<size_t BUCKET_SIZE>
-bool Segment<BUCKET_SIZE>::bucket_rebalance(unsigned int buckID0) {
+template<class T, size_t SBUCKET_SIZE, T MAX_KEY>
+bool Segment<T, SBUCKET_SIZE, MAX_KEY>::bucket_rebalance(unsigned int buckID0) {
     unsigned int buckID1 = buckID0 +1;
     //TODO
     return true;
 }
 
-template<size_t BUCKET_SIZE>
-uint64_t* Segment<BUCKET_SIZE>::lookup(key_type key) {
+template<class T, size_t SBUCKET_SIZE, T MAX_KEY>
+uint64_t* Segment<T, SBUCKET_SIZE, MAX_KEY>::lookup(T key) {
     value_type ret;
     return ret;
 }
 
-template<size_t BUCKET_SIZE>
-bool Segment<BUCKET_SIZE>::insert(KVPTR kvptr) {
+template<class T, size_t SBUCKET_SIZE, T MAX_KEY>
+bool Segment<T, SBUCKET_SIZE, MAX_KEY>::insert(KVPTR<T> kvptr) {
     return true;
 }
 
