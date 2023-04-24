e7c6cfc35d8403b5bc131a9323e971b399f30f8e
gdymind <gdymind@gmail.com>
2023-04-24 16:48:43 -0500

    update linear model unit tests

--- unittests/linear_model_unittest.cc
+++ unittests/linear_model_unittest.cc
@@ -30,25 +30,29 @@ namespace buckindex {
 
     TEST(LinearModel, get_endpoints_model) {
         LinearModel<uint64_t> m = LinearModel<uint64_t>::get_endpoints_model({0,2,4,6,8});
-        EXPECT_LT(abs(m.get_slope() - 0.5), 1e-6);
-        EXPECT_LT(abs(m.get_offset() - 0.0), 1e-6);
+        EXPECT_NEAR(m.get_slope(), 0.5, 1e-6);
+        EXPECT_NEAR(m.get_offset(),  0.0, 1e-6);
 
         LinearModel<uint64_t> m2 = LinearModel<uint64_t>::get_endpoints_model({10, 26});
-        EXPECT_LT(abs(m2.get_slope() - 1.0/16), 1e-6);
-        EXPECT_LT(abs(m2.get_offset() - -10.0/16), 1e-6);
+        EXPECT_NEAR(m2.get_slope(), 1.0/16, 1e-6);
+        EXPECT_NEAR(m2.get_offset(), -10.0/16, 1e-6);
+
+        LinearModel<uint64_t> m3 = LinearModel<uint64_t>::get_endpoints_model({10, 100});
+        EXPECT_NEAR(m3.get_slope(), 1.0/90, 1e-6);
+        EXPECT_NEAR(m3.get_offset(), -1.0/9, 1e-6);
     }
 
     TEST(LinearModel, get_regression_model) {
         LinearModel<uint64_t> m = LinearModel<uint64_t>::get_regression_model({0,2,4,6,8});
-        EXPECT_LT(abs(m.get_slope() - 0.5), 1e-6);
-        EXPECT_LT(abs(m.get_offset() - 0.0), 1e-6);
+        EXPECT_NEAR(m.get_slope(), 0.5, 1e-6);
+        EXPECT_NEAR(m.get_offset(), 0.0, 1e-6);
 
         LinearModel<uint64_t> m2 = LinearModel<uint64_t>::get_regression_model({10, 26});
-        EXPECT_LT(abs(m2.get_slope() - 1.0/16), 1e-6);
-        EXPECT_LT(abs(m2.get_offset() - -10.0/16), 1e-6);
+        EXPECT_NEAR(m2.get_slope(), 1.0/16, 1e-6);
+        EXPECT_NEAR(m2.get_offset(), -10.0/16, 1e-6);
 
         LinearModel<uint64_t> m3 = LinearModel<uint64_t>::get_regression_model({10, 17, 34, 38, 55, 66, 71, 82, 100});
-        EXPECT_LT(abs(m3.get_slope() - 0.09029), 1e-2);
-        EXPECT_LT(abs(m3.get_offset() - -0.7455), 1e-2);
+        EXPECT_NEAR(m3.get_slope(), 0.09029, 1e-3);
+        EXPECT_NEAR(m3.get_offset(), -0.7455, 1e-3);
     }
 }
