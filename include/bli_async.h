1f72ee938cd506d3794ad296b7062ba58c9eb134
gdymind <gdymind@gmail.com>
2023-06-15 15:47:55 -0500

    complied version of bli_concurrent

--- include/bli_async.h
+++ include/bli_async.h
@@ -1,12 +1,23 @@
 #include <queue>
 #include <future>
 #include <chrono>
-#include <thread>
+#include <pthread.h>
 
 #include "keyvalue.h"
 #include "buck_index.h"
 
 namespace  buckindex{
+
+enum RequestType {
+    read,
+    write
+};
+
+template<typename T, typename V>
+struct Request {
+    RequestType type;
+    KeyValue<T, V> kv;
+};
 
 // The Timer class would need to be defined by you to handle the timing of write requests
 class Timer {
@@ -22,26 +33,27 @@ public:
 
 template<typename T, typename V, size_t SEGMENT_BUCKET_SIZE, size_t DATA_BUCKET_SIZE>
 class BLI_async {
-    const size_t N_MAX_THREAD = 24;
+    static constexpr size_t N_MAX_THREAD = 24;
+    static constexpr int WRITE_ETA = 1000;
 public:
     BLI_async(size_t n_thread): n_thread_(n_thread) {
         //TODO: call idx.init(fill_ratio);
-        for (int i = 0; i < n) {
+        for (int i = 0; i < n_thread; i++) {
             int ret = pthread_create(&threads[i], nullptr, run, &i);
         }
     }
 
 private:
     size_t n_thread_ = 0; // number of background threads
-    BuckIndex<T, V, SEGMENT_BUCKET_SIZE, DATA_BUCKET_SIZE> idx;
+    static BuckIndex<T, V, SEGMENT_BUCKET_SIZE, DATA_BUCKET_SIZE> idx;
     pthread_t threads[N_MAX_THREAD]; // the first one is the RW thread
     // create n queues of n threads
-    std::queue<KeyValue<T, V>> input_queues_[N_MAX_THREAD];
+    std::queue<Request<T, V>> input_queues_[N_MAX_THREAD];
     std::queue<std::pair<KeyValue<T, V>, std::promise<int>>> O2M_queue; // other threads to main thread queue
 
-void run(void *arg) {
-    int idx = *((int *)arg);
-    std::queue<Request> &input_queue_ = input_queues_[idx];
+void* run(void *arg) {
+    int thread_id = *((int *)arg);
+    std::queue<KeyValue<T, V>> &input_queue_ = input_queues_[thread_id];
 
     std::promise<int> outstanding_write; // Only one outstanding write is allowed at a time
     std::future<int> write_future;
@@ -50,12 +62,12 @@ void run(void *arg) {
     Timer timer;
 
     while (!input_queue_.empty()) {
-        Request request = input_queue_.front();
+        KeyValue<T, V> request = input_queue_.front();
         input_queue_.pop();
-        int ret;
+        bool ret;
 
         if (request.type == read) {
-            ret = idx.read(request);
+            ret = idx.lookup(request.key_, request.value_);
         } else { // request.type == write
             if (idx == 0) { // main thread
                 ret = idx.write(request);
@@ -82,7 +94,7 @@ void run(void *arg) {
         while (!O2M_queue.empty()) { // check the O2M_queue for all the write request from all other threads.
             auto& [O2M_write_request, O2M_write_promise] = O2M_queue.front(); // get the write_request and write_promise from the queue.
             O2M_queue.pop();
-            int M2O_ret = bli_instance.write(O2M_write_request);
+            int M2O_ret = idx.insert(O2M_write_request.kv);
             O2M_write_promise.set_value(M2O_ret); // this will unblock the thread O's write_future.get();
         }
     }
--- include/bli_concurrent.h
+++ include/bli_concurrent.h
@@ -1,19 +1,30 @@
 #include <queue>
 #include <future>
 #include <chrono>
-#include <thread>
+#include <pthread.h>
 
 #include "keyvalue.h"
 #include "buck_index.h"
 
 namespace  buckindex{
 
+template<typename T, typename V, size_t SEGMENT_BUCKET_SIZE, size_t DATA_BUCKET_SIZE>
+struct ThreadArgs {
+    std::queue<std::pair<KeyValue<T, V>, std::promise<int>>> *write_queue;
+    BuckIndex<T, V, SEGMENT_BUCKET_SIZE, DATA_BUCKET_SIZE> *idx;
+};
 
+/**
+ * Concurrent interface of BLI; used by GRE
+*/
 template<typename T, typename V, size_t SEGMENT_BUCKET_SIZE, size_t DATA_BUCKET_SIZE>
 class BLI_concurrent {
 public:
-    BLI_concurrent(): {
-        pthread_create(&write_worker, nullptr, BLI_run, &write_queue);
+    BLI_concurrent() {
+        ThreadArgs<T, V, SEGMENT_BUCKET_SIZE, DATA_BUCKET_SIZE> args;
+        args.write_queue = &write_queue;
+        args.idx = &idx;
+        pthread_create(&write_worker, nullptr, reinterpret_cast<void* (*)(void*)>(run), &args);
     }
 
     bool read(const T key, V& value) {
@@ -22,24 +33,27 @@ public:
 
     int write(const KeyValue<T, V> &kv, size_t idx) {
         std::promise<int> outstanding_write;
-        write_queue.push(make_pair(request, outstanding_write));
-        write_future = outstanding_write.get_future();
+        write_queue.push(make_pair(kv, outstanding_write));
+        std::future<int> write_future = outstanding_write.get_future();
         return write_future.get(); // wait for the previous write request to finish before sending another one.
     }
     
 private:
-    BuckIndex<T, V, SEGMENT_BUCKET_SIZE, DATA_BUCKET_SIZE> idx;
     pthread_t write_worker;
     std::queue<std::pair<KeyValue<T, V>, std::promise<int>>> write_queue; // other threads to main thread queue
+    BuckIndex<T, V, SEGMENT_BUCKET_SIZE, DATA_BUCKET_SIZE> idx;
 
-    static void run(void *arg) {
-        std::queue<std::pair<Request, std::promise<V>>> *write_queue = arg;
+    static void* run(void *arg_list) {
+        ThreadArgs<T, V, SEGMENT_BUCKET_SIZE, DATA_BUCKET_SIZE> *args = 
+            reinterpret_cast<ThreadArgs<T, V, SEGMENT_BUCKET_SIZE, DATA_BUCKET_SIZE>*>(arg_list);
+        std::queue<std::pair<KeyValue<T, V>, std::promise<int>>> *write_queue = args->write_queue;
+        BuckIndex<T, V, SEGMENT_BUCKET_SIZE, DATA_BUCKET_SIZE> *idx = args->idx;
         while(true){
             while (!write_queue->empty()) { // check the O2M_queue for all the write request from all other threads.
-                auto& [kv, write_promise] = O2M_queue.front(); // get the write_request and write_promise from the queue.
-                O2M_queue.pop();
-                int M2O_ret = idx.insert(kv);
-                O2M_write_promise.set_value(M2O_ret); // this will unblock the thread O's write_future.get();
+                auto& [kv, write_promise] = write_queue->front(); // get the write_request and write_promise from the queue.
+                write_queue->pop();
+                int M2O_ret = idx->insert(kv);
+                write_promise.set_value(M2O_ret); // this will unblock the thread O's write_future.get();
             }
             pthread_yield();
         }
