2a862a0e236b75a7a5588f7df8d7920b8bdf493f
gdymind <gdymind@gmail.com>
2023-06-22 09:25:41 -0500

    bli_concurrent: use tbb concurrent queue

--- include/bli_concurrent.h
+++ include/bli_concurrent.h
@@ -2,18 +2,14 @@
 #include <future>
 #include <chrono>
 #include <pthread.h>
+#include <tbb/concurrent_queue.h>
+#include <iostream>
+#include <thread>
 
 #include "keyvalue.h"
 #include "buck_index.h"
 
 namespace  buckindex{
-
-template<typename T, typename V, size_t SEGMENT_BUCKET_SIZE, size_t DATA_BUCKET_SIZE>
-struct ThreadArgs {
-    std::queue<std::pair<KeyValue<T, V>, std::promise<int>>> *write_queue;
-    BuckIndex<T, V, SEGMENT_BUCKET_SIZE, DATA_BUCKET_SIZE> *idx;
-    std::mutex *write_queue_mutex; // std::queue is not thread safe
-};
 
 /**
  * Concurrent interface of BLI; used by GRE
@@ -25,79 +21,60 @@ public:
     }
 
     ~BLI_concurrent() {
-        pthread_cancel(write_worker);
+        consumer_thread->join();
+        delete idx;
+        delete write_queue;
     }
 
     void init(double fill_ratio) {
-        idx.init(fill_ratio);
-
+        idx = new BuckIndex<T, V, SEGMENT_BUCKET_SIZE, DATA_BUCKET_SIZE>(fill_ratio);
+        write_queue = new tbb::concurrent_queue<std::pair<KeyValue<T, V>, std::promise<int>>>();
        
-        args.write_queue = &write_queue;
-        args.idx = &idx;
-        args.write_queue_mutex = &write_queue_mutex;
-        pthread_create(&write_worker, nullptr, reinterpret_cast<void* (*)(void*)>(run), &args);
+        consumer_thread = new std::thread(run, write_queue, idx);
     }
 
     void bulk_load(std::vector<KeyValue<T, V>> &kvs) {
-        idx.bulk_load(kvs);
+        idx->bulk_load(kvs);
     }
 
     bool lookup(const T key, V& value) {
-       return idx.lookup(key, value);
+       return idx->lookup(key, value);
     }
 
-    int insert(const KeyValue<T, V> &kv) {
+    bool insert(const KeyValue<T, V> &kv) {
         std::promise<int> outstanding_write;
         std::future<int> write_future = outstanding_write.get_future();
-        {
-            std::lock_guard<std::mutex> lock(write_queue_mutex);
-            write_queue.push(make_pair(kv, std::move(outstanding_write))); // std::promise does not support copying
-        }
-        return write_future.get(); // wait for the previous write request to finish before sending another one.
+        write_queue->push({kv, std::move(outstanding_write)}); // std::promise is not copyable, so we need to use std::move.
+        return write_future.get(); // block until the write is done
     }
 
     void print_lookup_stat(){
-        idx.print_lookup_stat();
+        idx->print_lookup_stat();
     }
 
     void dump() {
-        idx.dump();
+        idx->dump();
     }
     
 private:
-    ThreadArgs<T, V, SEGMENT_BUCKET_SIZE, DATA_BUCKET_SIZE> args;
-    pthread_t write_worker;
-    std::queue<std::pair<KeyValue<T, V>, std::promise<int>>> write_queue; // other threads to main thread queue
-    std::mutex write_queue_mutex; // std::queue is not thread safe
-    BuckIndex<T, V, SEGMENT_BUCKET_SIZE, DATA_BUCKET_SIZE> idx;
+    using PromiseWriteType = std::pair<KeyValue<T, V>, std::promise<int>>;
+    
+    tbb::concurrent_queue<PromiseWriteType> *write_queue; // other threads to main thread queue
+    BuckIndex<T, V, SEGMENT_BUCKET_SIZE, DATA_BUCKET_SIZE> *idx; // TODO: remove runtime stats updates in BLI, to avoid synchronization overhead.
 
-    static void* run(void *arg_list) {
-        ThreadArgs<T, V, SEGMENT_BUCKET_SIZE, DATA_BUCKET_SIZE> *args = 
-            reinterpret_cast<ThreadArgs<T, V, SEGMENT_BUCKET_SIZE, DATA_BUCKET_SIZE>*>(arg_list);
-        std::queue<std::pair<KeyValue<T, V>, std::promise<int>>> *write_queue = args->write_queue;
-        std::mutex *write_queue_mutex = args->write_queue_mutex;
-        BuckIndex<T, V, SEGMENT_BUCKET_SIZE, DATA_BUCKET_SIZE> *idx = args->idx;
+    std::thread *consumer_thread;
+
+    static void run(tbb::concurrent_queue<PromiseWriteType> *write_queue, BuckIndex<T, V, SEGMENT_BUCKET_SIZE, DATA_BUCKET_SIZE> *idx) {
+        std::pair<KeyValue<T, V>, std::promise<int>> kv_promise_pair;
 
         while(true){ //TODO: add condition variable to wake up the thread when there is a write request.
-            std::pair<KeyValue<T, V>, std::promise<int>> kv_promise_pair;
-            {
-                std::lock_guard<std::mutex> lock(*write_queue_mutex);
-                if (write_queue->empty()) {
-                    pthread_yield();
-                    continue;
-                }
-                kv_promise_pair = std::move(write_queue->front()); // get the write_request and write_promise from the queue.
-                write_queue->pop();
+            if(write_queue->try_pop(kv_promise_pair)){
+                auto& [kv, write_promise] = kv_promise_pair;
+                bool insert_ret = idx->insert(kv);
+                write_promise.set_value(insert_ret); // this will unblock the thread O's write_future.get();
             }
-
-            auto& [kv, write_promise] = kv_promise_pair;
-            int M2O_ret = idx->insert(kv);
-            write_promise.set_value(M2O_ret); // this will unblock the thread O's write_future.get();
-
-            pthread_yield();
+            std::this_thread::yield();
         }
-
-        return nullptr;
     }
 };
 
